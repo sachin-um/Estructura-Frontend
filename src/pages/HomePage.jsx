02362399400dc768d1553b6efca86264977c1e5c
Pubudu-Anuradha <pubudu.anuradha.satharasinghe@gmail.com>
2023-06-22 13:31:56 +0530

    Added Some Configurations and the first pages of the site.
    - Configured Starting Router and theme
    - Added Main website page folder structure
    - Added Pages(All incomplete)
      - Homepage
      - Sign In page
      - Forgot Password page
      - Sign Up main page and the homeowner signup page
    - Created the first version of the topbar component which sets the page
      title and renders a material ui Appbar

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import TopBar from "../components/TopBar"
+
+function Homepage() {
+
+    return <>
+        <TopBar title="Home"/>
+
+        Unleash your home’s potential with everything at your fingertips.
+    </>
+}
+
+export default Homepage;
---
+++ src/pages/ServiceProviderSignIn.jsx
@@ -0,0 +1,1 @@
+// TODO: Add Service Provider Sign In Page with 2 paths (service provider and retail store)
