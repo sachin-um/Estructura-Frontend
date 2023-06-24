e7e6c815fa31cea64179b3e0324fe7ac39e87fee
Pubudu-Anuradha <pubudu.anuradha.satharasinghe@gmail.com>
2023-06-24 11:53:30 +0530

    Changed build directory to be 'build' instead of 'dist'

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import svgrPlugin from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgrPlugin()],
+  build: {
+    outDir: 'build',
+  },
 });
