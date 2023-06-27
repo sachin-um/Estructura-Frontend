6b04e16222469f8a88a41e48ccce4c6495da121c
Pamudi Guruge <92705961+Pamudi00@users.noreply.github.com>
2023-06-27 23:26:50 +0530

    Did some alignment changes and alterations

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -27,13 +27,16 @@ function SignIn() {
         <Grid maxWidth='lg' minHeight='100vh'  container>
           <Grid item md={6} xs={0}
             style={{
+              position: 'relative',
+              top: '20px',
+              left: '-50px'
             }}
           >
             { <img
               width='100%'
-              height='100%'
-              src='/sign.jpeg'
-              alt='kella ko?'
+              height='80%'
+              src='/signin.png'
+              alt='signinImg'
              
             /> }
           </Grid>
@@ -48,13 +51,15 @@ function SignIn() {
                 <img 
                 height="40%"
                 width="40%"
-                src='/logo.jpg'
+                src='/Logo.png'
                 alt='logo'
-                style={{paddingLeft:'30%'}}
+                style={{paddingLeft:'30%',
+                marginTop: '30px'  
+              }}
                 
               />
               </div> }
-              <Typography variant='h5' sx={{ textAlign: "center",textTransform: "uppercase", color:"#435834",}} >
+              <Typography variant='h5' sx={{ textAlign: "center",textTransform: "uppercase", color:"#435834", marginTop: '30px' }} >
                 Welcome Back
               </Typography>
             
@@ -64,14 +69,14 @@ function SignIn() {
                   margin: "10px",
                   display: "flex",
                   flexDirection: "column",
-                  gap: "10px",
+                  gap: "10px"
                 }}
                 onSubmit={HandleSubmit}
               >
-                <TextField  InputProps={{ sx: { borderRadius: 2 } }}sx={{ width: 1,margin:1 }}type='email' name='email' label='Email' variant="filled" size="small" />
-                <TextField  InputProps={{ sx: { borderRadius: 2 } }}sx={{ width: 1,margin:1 }}type='password' name='password' label='Password'  variant="filled" size="small"/>
+                <TextField  InputProps={{ sx: { borderRadius: 2 } }}sx={{ width: 1,margin:1 }}type='email' name='email' label='Email' variant="filled" size="small" color='secondary'/>
+                <TextField  InputProps={{ sx: { borderRadius: 2 } }}sx={{ width: 1,margin:1 }}type='password' name='password' label='Password'  variant="filled" size="small" color='secondary'/>
                 <Stack spacing={18} direction='row'>
-                <Link  href='/Register'  color="secondary" underline="hover">Don't have an account? Register</Link> 
+                <Link  href='/Register'  color="secondary" underline="hover" sx={{ marginLeft: 'auto'}}>Don't have an account? Register</Link> 
                 <Link  href='/ForgotPassword'  color="secondary" underline="hover">Forgot Password?</Link>
                 </Stack>
    
