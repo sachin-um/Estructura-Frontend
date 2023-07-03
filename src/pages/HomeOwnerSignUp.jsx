a40caa3970ab4aedac0f46f11bfb65d61ce72a02
Pamudi Guruge <92705961+Pamudi00@users.noreply.github.com>
2023-07-03 17:18:29 +0530

    Changed the UI of homeowner signup page

--- src/pages/HomeOwnerSignUp.jsx
+++ src/pages/HomeOwnerSignUp.jsx
@@ -1,163 +1,175 @@
 import TopBar from "../components/TopBar";
-import {
-  Box,
-  Button,
-  Container,
-  Grid,
-  Stack,
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
-  Link,
-  TextField,
-  Typography,
-} from "@mui/material";
+import { Button, Container, Grid, TextField, Typography, Box, FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 
 function HomeOwnerSignUp() {
   return (
-    <div style={{ height: '100vh' }}>
+    <>
       <TopBar title='Sign up as a Homeowner' />
-        <div style={{backgroundImage:"url('/formBg.jpg')", backgroundSize: 'cover', backgroundPosition: 'center', minHeight:'100vh', display: 'flex', justifyContent:'center', alignItems:'center', padding: '100px 0'}}>
-          <div style={{padding:20, backgroundColor:"white", borderRadius:20, maxWidth:1000}}>
-            <Container>
-              <Grid maxWidth='lg' minHeight='100vh' container>
-                <Grid item md={6} xs={0}
+      <Container
+        maxWidth={false}
+        style={{ backgroundColor: '#f7f8f1', minHeight: '100vh', display: 'flex', alignItems: 'center' }}
+      >
+        <Grid container justifyContent="center" spacing={4}>
+          <Grid item xs={12} md={7} style={{ paddingTop: '2rem', paddingBottom: '2rem', marginTop: '2rem' }}>
+            <Grid
+              container
+              style={{
+                backgroundImage: 'url("/HomeOwnerBG.jpg")',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                borderRadius: '20px',
+                height: '100%',
+                display: 'flex',
+                alignItems: 'flex-end',
+              }}
+            >
+              <Grid item xs={12} style={{ paddingLeft: '4rem', paddingRight: '1rem', marginBottom: '2rem' }}>
+                <Typography
+                  variant="h4"
                   style={{
-                    position: 'relative',
-                    top: '120px',
-                    left: '-90px'
+                    color: '#ffffff',
+                    fontSize: '1.5rem',
+                    textAlign: 'left',
+                    lineHeight: '1',
+                    paddingBottom: '1rem',
+                    marginTop: 'auto',
                   }}
                 >
-                  { <img
-                      width='130%'
-                      height='70%'
-                      src='/homeowner.png'
-                      alt='homeownerSignup'
-                    />}
-                </Grid>
-                <Grid item md={6} xs={12}>
-                  <Container maxWidth='sm'
-                    sx={{
-                      display: 'flex',
-                      flexDirection: 'column'
-                    }}
-              >
-              { <Grid style={{display:"flex"}}>
-                  <img 
-                    height='40%'
-                    width='40%'
-                    src='/Logo.png'
-                    alt='logo'
-                    style={{paddingLeft: '30%',
-                    marginTop: '30px'
+                  Unleash your home’s potential
+                </Typography>
+                <Typography
+                  variant="h4"
+                  style={{
+                    color: '#ffffff',
+                    fontSize: '1.5rem',
+                    textAlign: 'left',
+                    lineHeight: '1',
                   }}
-                  />
-              </Grid>}   
-                
-                <Box
-                  component='form'
-                  sx={{
-                    margin: '10px',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    gap: '10px'
-                  }}
-                  //onSubmit={HandleSubmit}
                 >
-                
-                <TextField  InputProps={{ sx: { borderRadius: 2 } }}sx={{ width: 1,margin:1 }}type='email' name='email' label='Email' variant="filled" size="small" color='secondary'/>
-                <Box sx={{ display: 'flex', gap: '10px' }}>
-                  <TextField
-                    InputProps={{ sx: { borderRadius: 2 } }}
-                    sx={{ flex: '1', margin: 1 }}
-                    type='firstName'
-                    name='firstName'
-                    label='First Name'
-                    variant="filled"
-                    size="small"
-                    color="secondary"
-                  />
-                  <TextField
-                    InputProps={{ sx: { borderRadius: 2 } }}
-                    sx={{ flex: '1', margin: 1 }}
-                    type='lastName'
-                    name='lastName'
-                    label='Last Name'
-                    variant="filled"
-                    size="small"
-                    color="secondary"
-                  />
-                </Box>
-                <TextField  InputProps={{ sx: { borderRadius: 2 } }}sx={{ width: 1,margin:1 }}type='contactNo' name='contactNo' label='Contact Number' variant="filled" size="small" color='secondary'/>
-                <TextField  InputProps={{ sx: { borderRadius: 2 } }}sx={{ width: 1,margin:1 }}type='password' name='password' label='Password' variant="filled" size="small" color='secondary'/>
-                <TextField  InputProps={{ sx: { borderRadius: 2 } }}sx={{ width: 1,margin:1 }}type='confirmPassword' name='confirmPassword' label='Confirm Password' variant="filled" size="small" color='secondary'/>
-                <Typography variant='h8' sx={{ textAlign: "left", color:"#435834", marginTop: '-8px', marginLeft: '10px'}} > Address </Typography>
-                <Box sx={{ display: 'flex', gap: '10px' }}>
-                  <TextField
-                    InputProps={{ sx: { borderRadius: 2 } }}
-                    sx={{ flex: '1', margin: 1 }}
-                    type='houseNo'
-                    name='houseNo'
-                    label='House No'
-                    variant="filled"
-                    size="small"
-                    color="secondary"
-                  />
-                  <TextField
-                    InputProps={{ sx: { borderRadius: 2 } }}
-                    sx={{ flex: '1', margin: 1 }}
-                    type='lane'
-                    name='lane'
-                    label='Lane'
-                    variant="filled"
-                    size="small"
-                    color="secondary"
-                  />
-                </Box>
-
-                <FormControl variant="filled" sx={{ m:1, minWidth: 120 }}>
-                  <InputLabel id="selectDistrict" color='secondary'>Select District</InputLabel>
-                  <Select labelId="selectDistrict-label">
-                  <MenuItem value={1}>Ampara</MenuItem>
-                  <MenuItem value={2}>Anuradhapura</MenuItem>
-                  <MenuItem value={3}>Badulla</MenuItem>
-                  <MenuItem value={4}>Batticaloa</MenuItem>
-                  <MenuItem value={5}>Colombo</MenuItem>
-                  <MenuItem value={6}>Galle</MenuItem>
-                  <MenuItem value={7}>Gampaha</MenuItem>
-                  <MenuItem value={8}>Hambantota</MenuItem>
-                  <MenuItem value={9}>Jaffna</MenuItem>
-                  <MenuItem value={10}>Kalutara</MenuItem>
-                  <MenuItem value={11}>Kandy</MenuItem>
-                  <MenuItem value={12}>Kegalle</MenuItem>
-                  <MenuItem value={13}>Kilinochchi</MenuItem>
-                  <MenuItem value={14}>Kurunegala</MenuItem>
-                  <MenuItem value={15}>Mannar</MenuItem>
-                  <MenuItem value={16}>Matale</MenuItem>
-                  <MenuItem value={17}>Matara</MenuItem>
-                  <MenuItem value={18}>Monaragala</MenuItem>
-                  <MenuItem value={19}>Mullaitivu</MenuItem>
-                  <MenuItem value={20}>Nuwara Eliya</MenuItem>
-                  <MenuItem value={21}>Polonnaruwa</MenuItem>
-                  <MenuItem value={22}>Puttalam</MenuItem>
-                  <MenuItem value={23}>Ratnapura</MenuItem>
-                  <MenuItem value={24}>Trincomalee</MenuItem>
-                  <MenuItem value={25}>Vavuniya</MenuItem>
-                  </Select>
-                </FormControl>
-                <TextField  InputProps={{ sx: { borderRadius: 2 } }}sx={{ width: 1,margin:1 }}type='city' name='city' label='City' variant="filled" size="small" color='secondary'/>
-                <Button sx={{ width: 1/3, padding: 1, marginLeft: 18,borderRadius:2 ,marginTop:2}}type='submit' color="primary" variant="contained" size='large'>Sign Up</Button>
-                </Box>
-              </Container>
+                  with everything at your fingertips
+                </Typography>
+              </Grid>
             </Grid>
           </Grid>
-        </Container>
-
-          </div>
-        </div> 
-    </div>
+          <Grid item xs={12} md={5}>
+            <Grid
+              container
+              style={{
+                backgroundColor: '#ffffff',
+                boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+                borderRadius: '20px',
+                padding: '1rem 2rem 3rem',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                marginTop: '2rem',
+                marginBottom: '2rem',
+              }}
+            >
+              <Grid item xs={12} style={{ marginBottom: '1.5rem', display: 'flex', justifyContent: 'center' }}>
+                <img src="/Logo.png" alt="Logo" style={{ width: '40%' }} />
+              </Grid>
+              <Grid item xs={12} style={{ marginTop: '1rem' }}>
+                <form
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '1.5rem',
+                    width: '100%',
+                    maxWidth: '400px',
+                    margin: '0 auto',
+                  }}
+                >
+                  <TextField label="Email" type="email" name="email" fullWidth variant="filled" color='secondary' />
+                  <Box sx={{ display: 'flex', gap: '10px', marginLeft: '2px', marginRight: '2px' }}>
+                    <TextField
+                      sx={{ flex: '1', margin: '1px 1px 1px 0' }}
+                      type='firstName'
+                      name='firstName'
+                      label='First Name'
+                      variant="filled"
+                      size="small"
+                      color="secondary"
+                    />
+                    <TextField
+                      sx={{ flex: '1', margin: '1px 0 1px 1px' }}
+                      type='lastName'
+                      name='lastName'
+                      label='Last Name'
+                      variant="filled"
+                      size="small"
+                      color="secondary"
+                    />
+                  </Box>
+                  <TextField label="Contact Number" type="contactNo" name="contactNo" fullWidth variant="filled" color='secondary' />
+                  <TextField label="Password" type="password" name="password" fullWidth variant="filled" color='secondary' />
+                  <TextField label="Confirm Password" type="confirmPassword" name="confirmPassword" fullWidth variant="filled" color='secondary' />
+                  <Typography variant='h8' sx={{ textAlign: "left", color: "#435834" }}> Address </Typography>
+                  <Box sx={{ display: 'flex', gap: '10px', marginTop: '-10px' }}>
+                    <TextField
+                      sx={{ flex: '1', margin: '1px 1px 1px 0' }}
+                      type='houseNo'
+                      name='houseNo'
+                      label='House No'
+                      variant="filled"
+                      size="small"
+                      color="secondary"
+                    />
+                    <TextField
+                      sx={{ flex: '1', margin: '1px 0 1px 1px' }}
+                      type='lane'
+                      name='lane'
+                      label='Lane'
+                      variant="filled"
+                      size="small"
+                      color="secondary"
+                    />
+                  </Box>
+                  <FormControl variant="filled" sx={{ m: 1, minWidth: 120, width: '100%', marginLeft: 'auto' }}>
+                    <InputLabel id="selectDistrict" color='secondary'>Select District</InputLabel>
+                    <Select labelId="selectDistrict-label">
+                      <MenuItem value={1}>Ampara</MenuItem>
+                      <MenuItem value={2}>Anuradhapura</MenuItem>
+                      <MenuItem value={3}>Badulla</MenuItem>
+                      <MenuItem value={4}>Batticaloa</MenuItem>
+                      <MenuItem value={5}>Colombo</MenuItem>
+                      <MenuItem value={6}>Galle</MenuItem>
+                      <MenuItem value={7}>Gampaha</MenuItem>
+                      <MenuItem value={8}>Hambantota</MenuItem>
+                      <MenuItem value={9}>Jaffna</MenuItem>
+                      <MenuItem value={10}>Kalutara</MenuItem>
+                      <MenuItem value={11}>Kandy</MenuItem>
+                      <MenuItem value={12}>Kegalle</MenuItem>
+                      <MenuItem value={13}>Kilinochchi</MenuItem>
+                      <MenuItem value={14}>Kurunegala</MenuItem>
+                      <MenuItem value={15}>Mannar</MenuItem>
+                      <MenuItem value={16}>Matale</MenuItem>
+                      <MenuItem value={17}>Matara</MenuItem>
+                      <MenuItem value={18}>Monaragala</MenuItem>
+                      <MenuItem value={19}>Mullaitivu</MenuItem>
+                      <MenuItem value={20}>Nuwara Eliya</MenuItem>
+                      <MenuItem value={21}>Polonnaruwa</MenuItem>
+                      <MenuItem value={22}>Puttalam</MenuItem>
+                      <MenuItem value={23}>Ratnapura</MenuItem>
+                      <MenuItem value={24}>Trincomalee</MenuItem>
+                      <MenuItem value={25}>Vavuniya</MenuItem>
+                    </Select>
+                  </FormControl>
+                  <TextField label="City" type="city" name="city" fullWidth variant="filled" color='secondary' />
+                  <Button variant="contained" color="primary" type="submit" fullWidth>
+                    Sign Up
+                  </Button>
+                </form>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
   );
 }
+
 export default HomeOwnerSignUp;
