39d19ad3d83ea46d60be8f2ac02a2529c294faf3
Sachin Umayangana <60028389+sachin-um@users.noreply.github.com>
2023-07-03 12:15:09 +0530

    Revert "Changed the UI of the sign in page"

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,10 +1,8 @@
-import { useState } from "react";
-import { AppBar, Container, Toolbar, Link, Box } from "@mui/material";
+import { AppBar, Container, Toolbar } from "@mui/material";
 import { Helmet } from "react-helmet";
-import { Link as RouterLink, useLocation } from "react-router-dom";
-import logo from "/Logo.png";
+import { Link } from "react-router-dom";
 
-const pages = [
+let pages = [
   { id: 0, title: "Estructura", link: "/" },
   { id: 1, title: "Sign In", link: "/SignIn" },
   { id: 2, title: "SignUp", link: "/SignUp" },
@@ -12,76 +10,27 @@ const pages = [
 ];
 
 function TopBar(props) {
-  const [activeTab, setActiveTab] = useState(null);
-  const location = useLocation();
-
   const title = props.title
     ? props.title
     : "Estructura: Creating Homes; Connecting Experts";
 
-  const handleTabChange = (pageId) => {
-    setActiveTab(pageId);
-  };
-
-  const isTabActive = (pageId) => {
-    return activeTab === pageId || location.pathname === pages[pageId].link;
-  };
-
+  // TODO: Add Logo Properly
+  // TODO: Create Sections for links(Home,Professionals etc) and User stuff(Login,Logout etc..)
+  // TODO: Add Search Bar?
+  // TODO: Style where necessary
   return (
     <>
       <Helmet>
         <title>{title}</title>
       </Helmet>
-      <AppBar position="sticky" sx={{ height: 80, backgroundColor: "#fff" }}>
+      <AppBar position='sticky'>
         <Container>
-          <Toolbar
-            sx={{
-              display: "flex",
-              justifyContent: "space-between",
-              height: "100%",
-            }}
-          >
-            <Box
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                marginLeft: -10,
-              }}
-            >
-              <RouterLink to="/">
-                <img src={logo} alt="Logo" style={{ height: 70 }} />
-              </RouterLink>
-            </Box>
-            <Box sx={{ display: "flex", alignItems: "center" }}>
-              {pages.map((page) => (
-                <Link
-                  key={page.id}
-                  component={RouterLink}
-                  to={page.link}
-                  color="inherit"
-                  underline={isTabActive(page.id) ? "always" : "none"}
-                  onMouseEnter={() => handleTabChange(page.id)}
-                  onMouseLeave={() => handleTabChange(null)}
-                  sx={{
-                    fontSize: "1rem",
-                    fontWeight: "bold",
-                    fontFamily: "Arial",
-                    color: isTabActive(page.id) ? "#2E8B57" : "#435834",
-                    marginLeft: 10,
-                    "&:hover": {
-                      color: "#2E8B57",
-                    },
-                    ...(isTabActive(page.id) && {
-                      textDecoration: "underline",
-                      textUnderlineOffset: "3px",
-                      transform: "translateY(-2px)",
-                    }),
-                  }}
-                >
-                  {page.title}
-                </Link>
-              ))}
-            </Box>
+          <Toolbar sx={{ display: { flex: "flex", gap: 10 } }}>
+            {pages.map((page) => (
+              <Link key={page.id} to={page.link}>
+                {page.title}
+              </Link>
+            ))}
           </Toolbar>
         </Container>
       </AppBar>
@@ -90,5 +39,5 @@ function TopBar(props) {
 }
 
 export default TopBar;
-export { pages };
 
+export { pages };
