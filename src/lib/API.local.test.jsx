06b1dbe17b01800abcc6e35f4e05a8871259a1b4
Pubudu-Anuradha <pubudu.anuradha.satharasinghe@gmail.com>
2023-07-04 13:35:31 +0530

    Added Request and response interceptors and tests

---
+++ src/lib/API.local.test.jsx
@@ -0,0 +1,35 @@
+import API from "./API";
+
+it("Gets Accesstoken from API", async () => {
+  const response = await API.post("/auth/authenticate", {
+    email: "admin@gmail.com",
+    password: "password",
+  });
+
+  expect(response.status).toBe(200);
+  expect(response.data.access_token).toBeTruthy();
+  expect(response.data.refresh_token).toBeTruthy();
+
+  localStorage.setItem("accessToken", response.data.access_token);
+  localStorage.setItem("refreshToken", response.data.refresh_token);
+});
+
+it("Refreshes Accesstoken from API", async () => {
+  const response = await API.post("/auth/refresh-token");
+  expect(response.status).toBe(200);
+  expect(response.data.access_token).toBeTruthy();
+  expect(response.data.refresh_token).toBeTruthy();
+  localStorage.setItem("accessToken", response.data.access_token);
+  localStorage.setItem("refreshToken", response.data.refresh_token);
+});
+
+it("waits and retries to get refreshed token from API", async () => {
+  setTimeout(async () => {
+    const response = await API.post("/auth/refresh-token");
+    expect(response.status).toBe(200);
+    expect(response.data.access_token).toBeTruthy();
+    expect(response.data.refresh_token).toBeTruthy();
+    localStorage.setItem("accessToken", response.data.access_token);
+    localStorage.setItem("refreshToken", response.data.refresh_token);
+  }, 10000).unref();
+}, 15000);
