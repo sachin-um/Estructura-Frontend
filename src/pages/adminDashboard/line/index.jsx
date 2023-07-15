4225d7f8d9b76ad2fef6871f027c42aee77a3a89
Saneru Akarawita <saneru.akarawita@gmail.com>
2023-07-15 08:56:43 +0530

    components of admin and form validation using formik and yup

---
+++ src/pages/adminDashboard/line/index.jsx
@@ -0,0 +1,17 @@
+import { Box } from "@mui/material";
+import Header from "../../../components/adminDashboard/Header";
+import LineChart from "../../../components/adminDashboard/LineChart";
+import "../../../assets/admindb.css"
+
+const Line = () => {
+  return (
+    <Box m="20px">
+      <Header title="Line Chart" subtitle="Simple Line Chart" />
+      <Box height="75vh">
+        <LineChart />
+      </Box>
+    </Box>
+  );
+};
+
+export default Line;
