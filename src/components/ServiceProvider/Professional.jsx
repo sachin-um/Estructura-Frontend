ed85330e956b783f16e0d64e1e0f91aa598234a2
Dewmini-Vithanage <2020cs194@stu.ucsc.cmb.ac.lk>
2023-07-16 19:20:50 +0530

    Architect signup

--- src/components/ServiceProvider/Professional.jsx
+++ src/components/ServiceProvider/Professional.jsx
@@ -1,4 +1,3 @@
-
 import {
   Box,
   Button,
@@ -15,100 +14,195 @@ import {
 } from "@mui/material";
 // import { Link } from "react-router-dom" ;
 
-function Professional() {
+function Professional({ handleDropdownChange }) {
+  // const [category, setCategory] = React.useState("");
   const HandleSubmit = (event) => {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
     console.log(formData.get("email"), formData.get("password"));
   };
 
+  const handleCategorySelection = (e) => {
+    handleDropdownChange(e.target.value);
+  };
+  // const handleCategorySelection=(e)=>{
+  //   setCategory(e);
+  // }
+
   // TODO: Change Layout
   return (
     <>
-      
-     
-       
-              <Box
-                component='form'
-                sx={{
-                  margin: "10px",
-                  display: "flex",
-                  flexDirection: "column",
-                  gap: "30px",
-                }}
-                onSubmit={HandleSubmit}
-              >
-               {<Grid style={{justifyContent:"center"}}>
-                <TextField  InputProps={{ sx: { borderRadius: 2 } }}sx={{ width: 1,margin:1 }}type='BusinessName' name='BusinessName' label='Business Name' variant="filled" size="small"  />
-                
-                <FormControl fullWidth variant="filled"  InputProps={{ sx: { borderRadius: 2 } }}sx={{ width: 1,margin:1 }}>
-                <InputLabel id="SelectProfessionalCategory">Professional Category</InputLabel>
-                <Select
-                    labelId="demo-simple-select-label"
-                
-                >
-                    <MenuItem value={1}>Architect</MenuItem>
-                    <MenuItem value={2}>Interior Designer</MenuItem>
-                    <MenuItem value={3}>Construction Company</MenuItem>
-                    <MenuItem value={4}>Home Builder</MenuItem>
-                    <MenuItem value={5}>Landscape Architect</MenuItem>
-                    <MenuItem value={6}>Painter</MenuItem>
-                    <MenuItem value={7}>Carpenter </MenuItem>
-                </Select>
-                </FormControl>
-                <TextField  InputProps={{ sx: { borderRadius: 2 } }}sx={{ width: 1,margin:1 }}type='contactNo' name='contactNo' label='Business Contact Number' variant="filled" size="small" color='secondary'/>
-               
-                <Typography variant='h8' sx={{ textAlign: "left", color:"#435834", marginTop: '-8px', marginLeft: '10px'}} > Address </Typography>
-                <Box sx={{ display: 'flex', gap: '10px' }}>
-                  <TextField InputProps={{ sx: { borderRadius: 2 } }} sx={{ flex: '1', margin: 1 }} type='houseNo'name='houseNo'label='House No'variant="filled"size="small"color="secondary"/>
-                  <TextField InputProps={{ sx: { borderRadius: 2 } }} sx={{ flex: '1', margin: 1 }} type='lane'name='lane'label='Lane'variant="filled"size="small" color="secondary"/>
-                </Box>
+      <Box
+        component="form"
+        sx={{
+          margin: "10px",
+          display: "flex",
+          flexDirection: "column",
+          gap: "30px",
+        }}
+        onSubmit={HandleSubmit}
+      >
+        {
+          <Grid style={{ justifyContent: "center" }}>
+            <TextField
+              InputProps={{ sx: { borderRadius: 2 } }}
+              sx={{ width: 1, margin: 1 }}
+              type="BusinessName"
+              name="BusinessName"
+              label="Business Name"
+              variant="filled"
+              size="small"
+            />
 
-                <FormControl fullWidth variant="filled" sx={{ m:1, minWidth: 120 }}>
-                  <InputLabel id="selectDistrict" color='secondary'>District</InputLabel>
-                  <Select labelId="selectDistrict-label">
-                  <MenuItem value={1}>Ampara</MenuItem>
-                  <MenuItem value={2}>Anuradhapura</MenuItem>
-                  <MenuItem value={3}>Badulla</MenuItem>
-                  <MenuItem value={4}>Batticaloa</MenuItem>
-                  <MenuItem value={5}>Colombo</MenuItem>
-                  <MenuItem value={6}>Galle</MenuItem>
-                  <MenuItem value={7}>Gampaha</MenuItem>
-                  <MenuItem value={8}>Hambantota</MenuItem>
-                  <MenuItem value={9}>Jaffna</MenuItem>
-                  <MenuItem value={10}>Kalutara</MenuItem>
-                  <MenuItem value={11}>Kandy</MenuItem>
-                  <MenuItem value={12}>Kegalle</MenuItem>
-                  <MenuItem value={13}>Kilinochchi</MenuItem>
-                  <MenuItem value={14}>Kurunegala</MenuItem>
-                  <MenuItem value={15}>Mannar</MenuItem>
-                  <MenuItem value={16}>Matale</MenuItem>
-                  <MenuItem value={17}>Matara</MenuItem>
-                  <MenuItem value={18}>Monaragala</MenuItem>
-                  <MenuItem value={19}>Mullaitivu</MenuItem>
-                  <MenuItem value={20}>Nuwara Eliya</MenuItem>
-                  <MenuItem value={21}>Polonnaruwa</MenuItem>
-                  <MenuItem value={22}>Puttalam</MenuItem>
-                  <MenuItem value={23}>Ratnapura</MenuItem>
-                  <MenuItem value={24}>Trincomalee</MenuItem>
-                  <MenuItem value={25}>Vavuniya</MenuItem>
-                  </Select>
-                </FormControl>
-                <TextField  InputProps={{ sx: { borderRadius: 2 } }}sx={{ width: 1,margin:1 }}type='city' name='city' label='City' variant="filled" size="small" color='secondary'/>
-                <Box sx={{ display: 'flex', gap: '10' }}>
-                  <TextField InputProps={{ sx: { borderRadius: 2 } }}sx={{ flex: '1', margin: 1 }}type='firstName'name='firstName'label='First Name'variant="filled"size="small"color="secondary"/>
-                  <TextField InputProps={{ sx: { borderRadius: 2 } }} sx={{ flex: '1', margin: 1 }}type='lastName'name='lastName'label='Last Name'variant="filled"size="small"color="secondary"/>
-                </Box>
-                </Grid >}
-    
-   
-                {/* { <Grid style={{display:"flex",justifyContent:"center",margin:10}}>
+            <FormControl
+              fullWidth
+              variant="filled"
+              InputProps={{ sx: { borderRadius: 2 } }}
+              sx={{ width: 1, margin: 1 }}
+            >
+              <InputLabel id="SelectProfessionalCategory">
+                Professional Category
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                onChange={handleCategorySelection}
+              >
+                <MenuItem value={"architect"}>Architect</MenuItem>
+                <MenuItem value={"interiordesigner"}>
+                  Interior Designer
+                </MenuItem>
+                <MenuItem value={"constructioncompany"}>
+                  Construction Company
+                </MenuItem>
+                <MenuItem value={"homebuilder"}>Home Builder</MenuItem>
+                <MenuItem value={"landscapearchitect"}>
+                  Landscape Architect
+                </MenuItem>
+                <MenuItem value={"painter"}>Painter</MenuItem>
+                <MenuItem value={"carpenter"}>Carpenter </MenuItem>
+              </Select>
+            </FormControl>
+            <TextField
+              InputProps={{ sx: { borderRadius: 2 } }}
+              sx={{ width: 1, margin: 1 }}
+              type="contactNo"
+              name="contactNo"
+              label="Business Contact Number"
+              variant="filled"
+              size="small"
+              color="secondary"
+            />
+
+            <Typography
+              variant="h8"
+              sx={{
+                textAlign: "left",
+                color: "#435834",
+                marginTop: "-8px",
+                marginLeft: "10px",
+              }}
+            >
+              {" "}
+              Address{" "}
+            </Typography>
+            <Box sx={{ display: "flex", gap: "10px" }}>
+              <TextField
+                InputProps={{ sx: { borderRadius: 2 } }}
+                sx={{ flex: "1", margin: 1 }}
+                type="houseNo"
+                name="houseNo"
+                label="House No"
+                variant="filled"
+                size="small"
+                color="secondary"
+              />
+              <TextField
+                InputProps={{ sx: { borderRadius: 2 } }}
+                sx={{ flex: "1", margin: 1 }}
+                type="lane"
+                name="lane"
+                label="Lane"
+                variant="filled"
+                size="small"
+                color="secondary"
+              />
+            </Box>
+
+            <FormControl
+              fullWidth
+              variant="filled"
+              sx={{ m: 1, minWidth: 120 }}
+            >
+              <InputLabel id="selectDistrict" color="secondary">
+                District
+              </InputLabel>
+              <Select labelId="selectDistrict-label">
+                <MenuItem value={1}>Ampara</MenuItem>
+                <MenuItem value={2}>Anuradhapura</MenuItem>
+                <MenuItem value={3}>Badulla</MenuItem>
+                <MenuItem value={4}>Batticaloa</MenuItem>
+                <MenuItem value={5}>Colombo</MenuItem>
+                <MenuItem value={6}>Galle</MenuItem>
+                <MenuItem value={7}>Gampaha</MenuItem>
+                <MenuItem value={8}>Hambantota</MenuItem>
+                <MenuItem value={9}>Jaffna</MenuItem>
+                <MenuItem value={10}>Kalutara</MenuItem>
+                <MenuItem value={11}>Kandy</MenuItem>
+                <MenuItem value={12}>Kegalle</MenuItem>
+                <MenuItem value={13}>Kilinochchi</MenuItem>
+                <MenuItem value={14}>Kurunegala</MenuItem>
+                <MenuItem value={15}>Mannar</MenuItem>
+                <MenuItem value={16}>Matale</MenuItem>
+                <MenuItem value={17}>Matara</MenuItem>
+                <MenuItem value={18}>Monaragala</MenuItem>
+                <MenuItem value={19}>Mullaitivu</MenuItem>
+                <MenuItem value={20}>Nuwara Eliya</MenuItem>
+                <MenuItem value={21}>Polonnaruwa</MenuItem>
+                <MenuItem value={22}>Puttalam</MenuItem>
+                <MenuItem value={23}>Ratnapura</MenuItem>
+                <MenuItem value={24}>Trincomalee</MenuItem>
+                <MenuItem value={25}>Vavuniya</MenuItem>
+              </Select>
+            </FormControl>
+            <TextField
+              InputProps={{ sx: { borderRadius: 2 } }}
+              sx={{ width: 1, margin: 1 }}
+              type="city"
+              name="city"
+              label="City"
+              variant="filled"
+              size="small"
+              color="secondary"
+            />
+            <Box sx={{ display: "flex", gap: "10" }}>
+              <TextField
+                InputProps={{ sx: { borderRadius: 2 } }}
+                sx={{ flex: "1", margin: 1 }}
+                type="firstName"
+                name="firstName"
+                label="First Name"
+                variant="filled"
+                size="small"
+                color="secondary"
+              />
+              <TextField
+                InputProps={{ sx: { borderRadius: 2 } }}
+                sx={{ flex: "1", margin: 1 }}
+                type="lastName"
+                name="lastName"
+                label="Last Name"
+                variant="filled"
+                size="small"
+                color="secondary"
+              />
+            </Box>
+          </Grid>
+        }
+
+        {/* { <Grid style={{display:"flex",justifyContent:"center",margin:10}}>
                 <Button sx={{ width: 1/3,  borderRadius:2 }}type='submit' color="primary" variant="contained" size='large'  href=''>Next</Button>
                 </Grid> } */}
-                
-              </Box>
-
-    
+      </Box>
     </>
   );
 }
--- src/pages/ServiceProviderSignUp.jsx
+++ src/pages/ServiceProviderSignUp.jsx
@@ -3,7 +3,11 @@ import TopBar from "../components/TopBar";
 import SignUpPage1 from "../components/ServiceProvider/SignUpPage1";
 import SignUpPage2 from "../components/ServiceProvider/SignUpPage2";
 import ArchitectPage1 from "../components/ServiceProvider/ArchitectSignUp/ArchitectPage1";
-import React,{useState} from "react";
+import ArchitectPage2 from "../components/ServiceProvider/ArchitectSignUp/ArchitectPage2";
+import ArchitectPage3 from "../components/ServiceProvider/ArchitectSignUp/ArchitectPage3";
+import ArchitectPage4 from "../components/ServiceProvider/ArchitectSignUp/ArchitectPage4";
+import ArchitectPage5 from "../components/ServiceProvider/ArchitectSignUp/ArchitectPage5";
+import React, { useState } from "react";
 import Professional from "../components/ServiceProvider/Professional";
 import RetailStore from "../components/ServiceProvider/RetailStore";
 import RentalStore from "../components/ServiceProvider/Rental";
@@ -22,7 +26,7 @@ import {
 function ServiceProviderSignUp() {
   const [currentPage, setCurrentPage] = useState(1);
   const [formData, setFormData] = useState({});
-  const [selectedOption, setSelectedOption] = useState('');
+  const [selectedOption, setSelectedOption] = useState("");
   // const [activeTab, setActiveTab] = useState(1);
   const [value, setValue] = React.useState("one");
 
@@ -37,8 +41,8 @@ function ServiceProviderSignUp() {
     } else if (activeTab === 2) {
       return <RetailStore />;
     } else if (activeTab === 3) {
-    return <RentalStore />;
-  }
+      return <RentalStore />;
+    }
     return <Professional />;
   };
 
@@ -56,7 +60,7 @@ function ServiceProviderSignUp() {
 
   const handleDropdownChange = (value) => {
     setSelectedOption(value);
-    setCurrentPage(1); // Reset to the first page when dropdown changes
+    setCurrentPage(2); // Reset to the first page when dropdown changes
   };
 
   const handleSubmit = () => {
@@ -64,14 +68,56 @@ function ServiceProviderSignUp() {
     console.log(formData);
   };
 
-  let pages = [<SignUpPage1 updateFormData={updateFormData} handleDropdownChange={handleDropdownChange} nextPage={nextPage}/>,<SignUpPage2 updateFormData={updateFormData} nextPage={nextPage} previousPage={previousPage} />];
+  let pages = [
+    <SignUpPage1
+      updateFormData={updateFormData}
+      handleDropdownChange={handleDropdownChange}
+      nextPage={nextPage}
+    />,
+    <SignUpPage2
+      updateFormData={updateFormData}
+      handleDropdownChange={handleDropdownChange}
+      nextPage={nextPage}
+      previousPage={previousPage}
+    />,
+  ];
 
-  if (selectedOption === 'option1') {
-    pages.push(<Page2 updateFormData={updateFormData} />);
-  } else if (selectedOption === 'option2') {
+  if (selectedOption === "architect") {
+    pages.push(
+      <ArchitectPage1
+        updateFormData={updateFormData}
+        handleDropdownChange={handleDropdownChange}
+        nextPage={nextPage}
+        previousPage={previousPage}
+      />,
+      <ArchitectPage2
+        updateFormData={updateFormData}
+        handleDropdownChange={handleDropdownChange}
+        nextPage={nextPage}
+        previousPage={previousPage}
+      />,
+      <ArchitectPage3
+        updateFormData={updateFormData}
+        handleDropdownChange={handleDropdownChange}
+        nextPage={nextPage}
+        previousPage={previousPage}
+      />,
+      <ArchitectPage4
+        updateFormData={updateFormData}
+        handleDropdownChange={handleDropdownChange}
+        nextPage={nextPage}
+        previousPage={previousPage}
+      />,
+      <ArchitectPage5
+        updateFormData={updateFormData}
+        handleDropdownChange={handleDropdownChange}
+        nextPage={nextPage}
+        previousPage={previousPage}
+      />
+    );
+  } else if (selectedOption === "option2") {
     pages.push(<Page3 updateFormData={updateFormData} />);
   }
-
 
   const HandleSubmit = (event) => {
     event.preventDefault();
@@ -82,7 +128,7 @@ function ServiceProviderSignUp() {
   // TODO: Change Layout
   return (
     <>
-      <TopBar title='Sign In to Estructura' />
+      <TopBar title="Sign In to Estructura" />
 
       {pages[currentPage - 1]}
       {/* {currentPage > 1 && (
