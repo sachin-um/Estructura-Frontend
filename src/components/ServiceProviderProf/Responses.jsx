bd3a38f660226c6351a8d213a9661ab01d276153
Pamudi Guruge <92705961+Pamudi00@users.noreply.github.com>
2023-07-20 07:56:02 +0530

    Did some changes

--- src/components/ServiceProviderProf/Responses.jsx
+++ src/components/ServiceProviderProf/Responses.jsx
@@ -18,7 +18,7 @@ function Responses() {
       <Card sx={cardStyle}>
           <CardContent>
             <Typography variant='body1' color='#435834' style={{ lineHeight: '2rem' }}>
-            <span style={{ fontSize: 'inherit', lineHeight: 'inherit', fontSize: '2rem'}}>&ldquo;</span>
+            <span style={{ lineHeight: 'inherit', fontSize: '2rem'}}>&ldquo;</span>
               The heart of the design can be in seamless integration with smart technology.
             From intelligent lighting and climate control systems that optimize energy consumption,
             enabling a truly dynamic workplace. We can focus on incorporating biophilic elements,
@@ -31,7 +31,7 @@ function Responses() {
         <Card sx={{...cardStyle, marginTop: '3rem'}}>
           <CardContent>
             <Typography variant='body1' color='#435834' style={{ lineHeight: '2rem' }}>
-            <span style={{ fontSize: 'inherit', lineHeight: 'inherit', fontSize: '2rem'}}>&ldquo;</span>
+            <span style={{ lineHeight: 'inherit', fontSize: '2rem'}}>&ldquo;</span>
               The design could focus on seamlessly integrating biophilic elements throughout the complex 
               to foster a deep connection betweent the inhabitants and the natural world. Green rooftops 
               could adorn the structures which not only adds relaxation to the residents, but also reduce 
