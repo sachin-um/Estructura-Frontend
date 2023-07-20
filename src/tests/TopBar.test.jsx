359f66ad13a1f2af69b16b0a1c1ae116d6966148
Saneru Akarawita <saneru.akarawita@gmail.com>
2023-07-20 19:22:41 +0530

    remove topbartest file

--- src/tests/TopBar.test.jsx
+++ src/tests/TopBar.test.jsx
@@ -2,15 +2,3 @@
  * @jest-environment jsdom
  */
 
-import "@testing-library/jest-dom";
-import { render} from "@testing-library/react";
-import TopBar from "../components/TopBar";
-import { BrowserRouter } from "react-router-dom";
-
-it("renders the text", () => {
-  render(
-    <BrowserRouter>
-      <TopBar />
-    </BrowserRouter>
-  );
-});
