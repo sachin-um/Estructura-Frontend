0b90fac9d4704af4ac12002e94abf5dc8c86eddd
Saneru Akarawita <saneru.akarawita@gmail.com>
2023-07-20 19:27:01 +0530

    check again

--- src/tests/TopBar.test.jsx
+++ src/tests/TopBar.test.jsx
@@ -2,21 +2,13 @@
  * @jest-environment jsdom
  */
 
-import React from "react";
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-import TopBar from "../components/TopBar";
+import { render} from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 
-jest.mock("axios");
-
-it("renders the TopBar component without errors", () => {
+it("renders the logo text", () => {
   render(
     <BrowserRouter>
-      <TopBar />
     </BrowserRouter>
   );
-
-  // Check if the TopBar component renders without errors
-  expect(screen.getByTestId("top-bar")).toBeInTheDocument();
 });
