6ee6d8598415c52c84aa7e2257ec4a540fbb134c
Pamudi Guruge <92705961+Pamudi00@users.noreply.github.com>
2023-07-23 22:45:23 +0530

    Reviews UI in the Service Provider Profile

--- src/components/ServiceProviderProf/Reviews.jsx
+++ src/components/ServiceProviderProf/Reviews.jsx
@@ -1,18 +1,50 @@
 
-import { Button, Container, Grid, TextField, Typography, Box, FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import { Grid, Typography, Card, CardContent, Avatar, Rating } from '@mui/material';
 import { Link } from 'react-router-dom';
-
-
-
-
 
 function Reviews() {
   return (
     <>
-     <Grid style={{display:"flex",justifyContent:"center", marginTop: '3rem',marginLeft: '10rem'  }}>
-    <Typography variant='h6'> Reviews </Typography>
-     </Grid>
-     
+    <Card style={{width: '80%', margin: '2rem auto' }}>
+      <CardContent>
+        <Grid container spacing={2} alignItems='center'>
+          <Grid item>
+            <Avatar />
+          </Grid>
+          <Grid item>
+            <Typography variant='h7'>User Name</Typography>
+          </Grid>
+        </Grid>
+        <Typography variant='body1' style={{ marginTop: '1.5rem' }}>
+            I am very happy with the service I received from Urban Architecture. It was delightful to work with them and they provided an excellent service.
+        </Typography>
+        <Rating name='rating' value={3.5} precision={0.5} readOnly style={{ marginTop: '1.5rem' }}/>
+        <Typography variant='subtitle2' color='textSecondary' style={{marginTop: '1rem'}}>
+          10/07/2023 at 12:34 PM
+        </Typography>
+      </CardContent>
+    </Card>
+
+    <Card style={{width: '80%', margin:'2rem auto' }}>
+      <CardContent>
+        <Grid container spacing={2} alignItems='center'>
+          <Grid item>
+            <Avatar />
+          </Grid>
+          <Grid item>
+            <Typography variant='h7'>User Name</Typography>
+          </Grid>
+        </Grid>
+        <Typography variant='body1' style={{ marginTop: '1.5rem' }}>
+          It was an absoulte pleasure working with Urban Architecture. John was very professional and great to work with! He exceeded my expectations and I highly recommend him!
+        </Typography>
+        <Rating name='rating' value={4} readOnly style={{ marginTop: '1.5rem' }} />
+        <Typography variant='subtitle2' color='textSecondary' style={{marginTop: '1rem'}}>
+          02/05/2023 at 10:45 AM
+        </Typography>
+      </CardContent>
+    </Card>
+
     </>
   );
 }
