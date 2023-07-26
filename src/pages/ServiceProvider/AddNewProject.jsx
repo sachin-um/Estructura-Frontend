bae55e39205c67a5bdf6c2cb0a6cd3d7ca552023
Sachin Umayangana <sachinumayangana@gmail.com>
2023-07-26 14:35:23 +0530

    add project img uploading

--- src/pages/ServiceProvider/AddNewProject.jsx
+++ src/pages/ServiceProvider/AddNewProject.jsx
@@ -18,8 +18,21 @@ import {
 } from "@mui/material";
 
 function AddNewProject() {
-  const [image, setImage] = useState(null);
-  const [fileName, setFileName] = useState("No selected file");
+  const [images, setImages] = useState([]);
+  const [fileNames, setFileNames] = useState([]);
+  const placeholdersCount = images.length === 0 ? 3 :images.length >= 4 ? 0 : 3 - (images.length-1);
+  const placeholders=Array.from({ length: placeholdersCount }).fill(0);
+  const removeImage = (index) => {
+    const updatedImages = [...images];
+    updatedImages.splice(index, 1);
+    setImages(updatedImages);
+
+    const updatedFileNames = [...fileNames];
+    updatedFileNames.splice(index, 1);
+    setFileNames(updatedFileNames);
+  };
+  // const [image, setImage] = useState(null);
+  // const [fileName, setFileName] = useState("No selected file");
   return (
     <>
     <TopBar title="" />
@@ -67,7 +80,34 @@ function AddNewProject() {
             >
               Add images related to your project
             </Typography>
-
+            <Box
+              style={{ borderRadius: "5px" ,display: "flex",justifyContent: "center",}}
+              position="relative"
+              height="80px"
+              width="100%"
+              marginTop="20px"
+              sx={{ borderStyle: "dashed", borderColor: "grey" }}
+              onClick={() => document.querySelector(".input-field").click()}
+            >
+              <input
+                  type="file"
+                  accept="image/*"
+                  className="input-field"
+                  hidden
+                  multiple
+                  onChange={({ target: { files } }) => {
+                    if (files && files.length > 0) {
+                      const fileArray = Array.from(files).slice(0,4);
+                      const fileNames = fileArray.map(file => file.name);
+                      setFileNames(fileNames);
+                      const imageUrls = fileArray.map(file => URL.createObjectURL(file));
+                      setImages(imageUrls);
+                    }
+                  }}
+                />
+                <MdCloudUpload color="#1475cf" size={60} />
+                      <p>Browse Files to upload</p>
+            </Box>
             <Box
               style={{ borderRadius: "5px" }}
               position="relative"
@@ -76,32 +116,11 @@ function AddNewProject() {
               marginTop="20px"
               sx={{ borderStyle: "dashed", borderColor: "grey" }}
             >
-              <form
-                action=""
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                }}
-                onClick={() => document.querySelector(".input-field").click()}
-              >
-                <input
-                  type="file"
-                  accept="image/*"
-                  className="input-field"
-                  hidden
-                  onChange={({ target: { files } }) => {
-                    files[0] && setFileName(files[0].name);
-                    if (files) {
-                      setImage(URL.createObjectURL(files[0]));
-                    }
-                  }}
-                />
-
-                {image ? (
+                {images[0] ? (
                   <img
-                    src={image}
+                    src={images[0]}
                     style={{ width: "100%", height: "300px" }}
-                    alt={fileName}
+                    alt={fileNames[0]}
                   />
                 ) : (
                   <>
@@ -117,12 +136,85 @@ function AddNewProject() {
                     </Grid>
                   </>
                 )}
-              </form>
             </Box>
 
             {/* <Typography>Images</Typography> */}
 
             <Grid container spacing={1}>
+                {images.length > 1 && images.slice(1, 4).map((imageUrl, index) => (
+                  <Grid item xs={4} key={index}>
+                    <Box
+                      position="relative"
+                      height="150px"
+                      width="100%"
+                      marginTop="30px"
+                    >
+                      <img
+                        src={imageUrl}
+                        style={{ width: "100%", height: "150px" }}
+                        alt={fileNames[index + 1]}
+                      />
+                      <span
+                        style={{
+                          position: "absolute",
+                          top:"10px",
+                          right: "10px",
+                          backgroundColor:"red",
+                          color: "white",
+                          padding: "5px",
+                          cursor: "pointer"
+                        }}
+                        onClick={() => removeImage(index+1)}
+                      >
+                        Remove
+                      </span>
+                      </Box>
+                      </Grid>
+                    ))}
+                {placeholders.map((index) => (
+                  <Grid item xs={4} key={index}>
+                      <Box
+                        position="relative"
+                        height="150px"
+                        width="100%"
+                        marginTop="30px"
+                      >
+                        
+                          <Grid
+                            style={{
+                              display: "flex",
+                              justifyContent: "center",
+                              marginTop: "100px",
+                            }}
+                          >
+                            <MdCloudUpload color="#1475cf" size={60} />
+                            <p>Browse Files to upload</p>
+                          </Grid>
+                      </Box>
+                  </Grid>
+                ))}
+                
+                {/* <div key={index} className="image-container">
+                      <img
+                        src={imageUrl}
+                        style={{ width: "100%", height: "100%" }}
+                        alt={fileNames[index + 1]} // We add 1 to the index to match the correct fileName in the fileNames array.
+                      />
+                      <span
+                        className="remove-icon"
+                        onClick={() => {
+                          const updatedImages = [...images];
+                          updatedImages.splice(index + 1, 1); // We add 1 to the index to match the correct image in the images array.
+                          setImages(updatedImages);
+
+                          const updatedFileNames = [...fileNames];
+                          updatedFileNames.splice(index + 1, 1); // We add 1 to the index to match the correct fileName in the fileNames array.
+                          setFileNames(updatedFileNames);
+                        }}
+                      >
+                        Remove
+                      </span>
+                    </div>
               <Grid item xs={4}>
                 <Box
                   position="relative"
@@ -130,15 +222,26 @@ function AddNewProject() {
                   width="100%"
                   marginTop="30px"
                 >
+                  {images[1] ? (
                   <img
-                    src="formBg.jpg"
-                    alt="Banner"
-                    style={{
-                      width: "100%",
-                      height: "100%",
-                      objectFit: "cover",
-                    }}
+                    src={images[1]}
+                    style={{ width: "100%", height: "300px" }}
+                    alt={fileNames[1]}
                   />
+                ) : (
+                  <>
+                    <Grid
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        marginTop: "100px",
+                      }}
+                    >
+                      <MdCloudUpload color="#1475cf" size={60} />
+                      <p>Browse Files to upload</p>
+                    </Grid>
+                  </>
+                )}
                 </Box>
               </Grid>
               <Grid item xs={4}>
@@ -148,15 +251,26 @@ function AddNewProject() {
                   width="100%"
                   marginTop="30px"
                 >
+                  {images[1] ? (
                   <img
-                    src="formBg.jpg"
-                    alt="Banner"
-                    style={{
-                      width: "100%",
-                      height: "100%",
-                      objectFit: "cover",
-                    }}
+                    src={images[1]}
+                    style={{ width: "100%", height: "300px" }}
+                    alt={fileNames[1]}
                   />
+                ) : (
+                  <>
+                    <Grid
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        marginTop: "100px",
+                      }}
+                    >
+                      <MdCloudUpload color="#1475cf" size={60} />
+                      <p>Browse Files to upload</p>
+                    </Grid>
+                  </>
+                )}
                 </Box>
               </Grid>
               <Grid item xs={4}>
@@ -166,17 +280,28 @@ function AddNewProject() {
                   width="100%"
                   marginTop="30px"
                 >
+                  {images[1] ? (
                   <img
-                    src="formBg.jpg"
-                    alt="Banner"
-                    style={{
-                      width: "100%",
-                      height: "100%",
-                      objectFit: "cover",
-                    }}
+                    src={images[1]}
+                    style={{ width: "100%", height: "300px" }}
+                    alt={fileNames[1]}
                   />
+                ) : (
+                  <>
+                    <Grid
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        marginTop: "100px",
+                      }}
+                    >
+                      <MdCloudUpload color="#1475cf" size={60} />
+                      <p>Browse Files to upload</p>
+                    </Grid>
+                  </>
+                )}
                 </Box>
-              </Grid>
+              </Grid> */}
             </Grid>
             <Typography
               variant="h6"
