ddf5d085758662d45d157f2810ceaa364052fe01
Pubudu-Anuradha <pubudu.anuradha.satharasinghe@gmail.com>
2023-07-28 00:09:24 +0530

    more lint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,38 +1,37 @@
 module.exports = {
-  parser: "@babel/eslint-parser",
-  parserOptions: {
-    requireConfigFile: false,
-    babelOptions: {
-      presets: ["@babel/preset-react"],
-    },
-  },
-
   extends: [
+    'react-app',
     'plugin:prettier/recommended',
     'plugin:import/errors',
     'plugin:react/recommended',
     'plugin:perfectionist/recommended-natural',
   ],
-  plugins: ['prettier', 'unused-imports', 'react', 'perfectionist'],
+  parser: '@babel/eslint-parser',
+
+  parserOptions: {
+    babelOptions: {
+      presets: ['@babel/preset-react'],
+    },
+    requireConfigFile: false,
+  },
+  plugins: ['prettier', 'import', 'react', 'perfectionist'],
   root: true,
   rules: {
-    "react/react-in-jsx-scope": 0,
+    'import/no-unresolved': [2, { caseSensitive: false }],
     'no-unused-vars': 'off', // or "@typescript-eslint/no-unused-vars": "off",
     'prettier/prettier': 'error',
-    'unused-imports/no-unused-imports': 'error',
-    'unused-imports/no-unused-vars': [
-      'error',
-      {
-        args: 'after-used',
-        argsIgnorePattern: '^_',
-        vars: 'all',
-        varsIgnorePattern: '^_',
-      },
-    ],
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 0,
   },
   settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        paths: ['src', 'public'],
+      },
+    },
     react: {
       version: 'detect',
     },
   },
-}
+};
