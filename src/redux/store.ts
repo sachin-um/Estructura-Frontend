4fec6b8ee7eaada26ab04cd115dca54f12318de1
Sachin Umayangana <sachinumayangana@gmail.com>
2023-08-05 19:11:48 +0530

    resole conflict

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,23 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-<<<<<<< HEAD
-import BlogsReducer from './BlogsReducer';
-import ProjectReducer from './ProjectReducer';
-=======
 import BlogsReducer from './Blogs/BlogsReducer';
 import SingleBlogReducer from './Blogs/SingleBlogReducer';
->>>>>>> devel
 import UserAuthenticationReducer from './UserAuthenticationReducer';
 
 export const store = configureStore({
   reducer: {
-<<<<<<< HEAD
-    blog: BlogsReducer,
-    project: ProjectReducer,
-=======
     blog: SingleBlogReducer,
     blogs: BlogsReducer,
->>>>>>> devel
     user: UserAuthenticationReducer,
   },
 });
