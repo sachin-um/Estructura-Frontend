b7ed5b0cf11b81dee6fbad28a5b5079d64f5e286
Dewmini-Vithanage <2020cs194@stu.ucsc.cmb.ac.lk>
2023-08-10 14:27:39 +0530

    View Project finalized

--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -1,6 +1,6 @@
 import { type RouteObject } from 'react-router-dom';
 
-import AuthTest from '../AuthTest';
+// import AuthTest from '../AuthTest';
 import ForgotPassword from '../pages/ForgotPassword';
 import HomeOwnerSignUp from '../pages/HomeOwnerSignUp';
 import ServiceProviderSignUp from '../pages/ServiceProviderSignUp';
@@ -8,7 +8,7 @@ import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 
 const AuthRoutes: RouteObject[] = [
-  { path: '/auth', element: <AuthTest /> },
+  // { path: '/auth', element: <AuthTest /> },
   { path: '/SignIn', element: <SignIn /> },
   {
     path: '/ForgotPassword',
