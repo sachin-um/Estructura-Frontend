33cdf638efa4b8b9ea25c5071a53580f7e5f5e0c
Pubudu-Anuradha <pubudu.anuradha.satharasinghe@gmail.com>
2023-09-14 13:25:20 +0530

    Convert to typescript

---
+++ src/pages/customerRequests/AddCustomerRequest.tsx
@@ -0,0 +1,495 @@
+import CloseIcon from '@mui/icons-material/Close';
+import {
+  Avatar,
+  Box,
+  Button,
+  ButtonBase,
+  Container,
+  Grid,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { useRef, useState } from 'react';
+import {
+  MdDescription,
+  MdInsertDriveFile,
+  MdPictureAsPdf,
+} from 'react-icons/md';
+
+import '../../assets/font.css';
+import Footer from '../../components/Footer';
+import TopAppBar from '../../components/TopAppBar';
+
+const AddCustomerRequest = () => {
+  const [selectedCategories, setSelectedCategories] = useState<
+    (RetailItemType | Role)[]
+  >([]);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [uploadedImages, setUploadedImages] = useState<File[]>([]);
+  const [uploadedDocuments, setUploadedDocuments] = useState<File[]>([]);
+
+  const handleCategoryClick = (categoryId: RetailItemType | Role) => {
+    if (selectedCategories.includes(categoryId)) {
+      setSelectedCategories(
+        selectedCategories.filter((item) => item !== categoryId),
+      );
+    } else {
+      setSelectedCategories([...selectedCategories, categoryId]);
+    }
+  };
+
+  const handleUploadClick = () => {
+    if (fileInputRef.current) fileInputRef.current.click();
+  };
+
+  const handleFileChange:
+    | React.ChangeEventHandler<HTMLInputElement>
+    | undefined = (event) => {
+    const files = event.target.files;
+    if (files !== null) {
+      const filesArray = Array.from(files);
+      const imagesArray = filesArray.filter((file) =>
+        file.type.includes('image'),
+      );
+      const documentsArray = filesArray.filter(
+        (file) => !file.type.includes('image'),
+      );
+
+      setUploadedImages((prevImages) => [...prevImages, ...imagesArray]);
+      setUploadedDocuments((prevDocuments) => [
+        ...prevDocuments,
+        ...documentsArray,
+      ]);
+    }
+  };
+
+  const handleRemoveImage = (index: number) => {
+    setUploadedImages((prevImages) => prevImages.filter((_, i) => i !== index));
+  };
+
+  const handleRemoveDocument = (index: number) => {
+    setUploadedDocuments((prevDocuments) =>
+      prevDocuments.filter((_, i) => i !== index),
+    );
+  };
+
+  const isCategorySelected = (categoryId: RetailItemType | Role) =>
+    selectedCategories.includes(categoryId);
+
+  const categoryList: { id: Role; imageSrc: string; text: string }[] = [
+    {
+      id: 'ARCHITECT',
+      imageSrc:
+        'https://designeverest.com/wp-content/uploads/2023/01/what-is-the-role-of-an-architect-in-building-a-house-1629876140.jpg',
+      text: 'Architects',
+    },
+    {
+      id: 'CONSTRUCTIONCOMPANY',
+      imageSrc:
+        'https://www.crystalconstruction.lk/wp-content/uploads/2023/05/The-Benefits-of-Hiring-a-Professional-Construction-Company-in-Sri-Lanka.png',
+      text: 'Construction Companies',
+    },
+    {
+      id: 'INTERIORDESIGNER',
+      imageSrc:
+        'https://www.myperfectresume.com/wp-content/uploads/2020/09/how-to-become-an-interior-designer.jpg',
+      text: 'Interior Designers',
+    },
+    {
+      id: 'LANDSCAPEARCHITECT',
+      imageSrc:
+        'https://design.asu.edu/sites/default/files/2022-07/landscape-drawing-pencil.jpg',
+      text: 'Landscape Architects',
+    },
+    {
+      id: 'MASONWORKER',
+      imageSrc:
+        'https://www.ibuildnew.com.au/blog/wp-content/uploads/2018/06/Builder-looking-up.png',
+      text: 'Home Builders',
+    },
+    {
+      id: 'CARPENTER',
+      imageSrc:
+        'https://generisonline.com/wp-content/uploads/2022/05/The-difference-between-a-joiner-and-a-carpenter-1.jpg',
+      text: 'Carpenters',
+    },
+    {
+      id: 'PAINTER',
+      imageSrc:
+        'https://blog.renovationfind.com/wp-content/uploads/2019/01/Painter-in-Edmonton.jpg',
+      text: 'Painters',
+    },
+  ];
+
+  const retailItemList: {
+    id: RetailItemType;
+    imageSrc: string;
+    text: string;
+  }[] = [
+    {
+      id: 'FURNITURE',
+      imageSrc:
+        'https://cdn-eu.dynamicyield.com/api/9877108/images/24d111bf06dd1__furniture.jpg',
+      text: 'Furniture',
+    },
+    {
+      id: 'HARDWARE',
+      imageSrc:
+        'https://images.pexels.com/photos/220639/pexels-photo-220639.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      text: 'Hardware',
+    },
+    {
+      id: 'BATHWARE',
+      imageSrc:
+        'https://images.pexels.com/photos/11125357/pexels-photo-11125357.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      text: 'Bathware',
+    },
+    {
+      id: 'GARDENWARE',
+      imageSrc:
+        'https://peakproducts-canada.s3.ca-central-1.amazonaws.com/gardenware-banner-img-2.jpeg',
+      text: 'Gardenware',
+    },
+    {
+      id: 'LIGHTING',
+      imageSrc:
+        'https://images.pexels.com/photos/1123262/pexels-photo-1123262.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      text: 'Lighting',
+    },
+  ];
+
+  return (
+    <>
+      <TopAppBar />
+      <Container
+        sx={{
+          alignItems: 'flex-start',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <Typography
+          color="#435834"
+          fontFamily="Poppins"
+          sx={{ marginTop: 10, textAlign: 'left' }}
+          variant="h4"
+        >
+          Transform Your Vision into a Reality.
+          <br />
+          Discover Your Perfect Match.
+        </Typography>
+
+        <Typography
+          fontFamily="Poppins"
+          fontSize="1.2rem"
+          sx={{ marginTop: 8, textAlign: 'left' }}
+        >
+          Request Title
+        </Typography>
+        <TextField
+          fullWidth
+          label="Your request in one sentence"
+          margin="normal"
+          variant="outlined"
+        />
+
+        <Typography
+          fontFamily="Poppins"
+          fontSize="1.2rem"
+          sx={{ TextAlign: 'left', marginBottom: 3, marginTop: 5 }}
+        >
+          What category/s of professional/s are you looking for?
+        </Typography>
+
+        <Grid container spacing={9}>
+          {categoryList.map((categoryItem) => (
+            <Grid item key={categoryItem.id}>
+              <ButtonBase
+                style={{
+                  alignItems: 'center',
+                  backgroundColor: 'transparent',
+                  border: isCategorySelected(categoryItem.id)
+                    ? '2px solid green'
+                    : 'none',
+                  boxSizing: 'border-box',
+                  cursor: 'pointer',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  height: '160px',
+                  padding: '4px',
+                  width: '100px',
+                }}
+                onClick={() => handleCategoryClick(categoryItem.id)}
+              >
+                <Avatar
+                  alt={categoryItem.text}
+                  src={categoryItem.imageSrc}
+                  sx={{ height: 90, width: 90 }}
+                />
+                <Typography
+                  align="center"
+                  sx={{ marginTop: 2 }}
+                  variant="body2"
+                >
+                  {categoryItem.text}
+                </Typography>
+              </ButtonBase>
+            </Grid>
+          ))}
+        </Grid>
+
+        <Typography
+          fontFamily="Poppins"
+          fontSize="1.2rem"
+          sx={{ TextAlign: 'left', marginBottom: 3, marginTop: 5 }}
+        >
+          What type/s of retail item/s are you looking for?
+        </Typography>
+
+        <Grid container spacing={10}>
+          {retailItemList.map((item) => (
+            <Grid item key={item.id}>
+              <ButtonBase
+                style={{
+                  alignItems: 'center',
+                  backgroundColor: 'transparent',
+                  border: isCategorySelected(item.id)
+                    ? '2px solid green'
+                    : 'none',
+                  boxSizing: 'border-box',
+                  cursor: 'pointer',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  height: '160px',
+                  padding: '4px',
+                  width: '100px',
+                }}
+                onClick={() => handleCategoryClick(item.id)}
+              >
+                <Avatar
+                  alt={item.text}
+                  src={item.imageSrc}
+                  sx={{ height: 90, width: 90 }}
+                />
+                <Typography
+                  align="center"
+                  sx={{ marginTop: 2 }}
+                  variant="body2"
+                >
+                  {item.text}
+                </Typography>
+              </ButtonBase>
+            </Grid>
+          ))}
+        </Grid>
+
+        <Typography
+          fontFamily="Poppins"
+          fontSize="1.2rem"
+          sx={{ TextAlign: 'left', marginBottom: 1, marginTop: 5 }}
+        >
+          Describe what you have in mind
+        </Typography>
+
+        <TextField
+          fullWidth
+          label="Your description"
+          margin="normal"
+          multiline
+          rows={6}
+          sx={{ width: '50%' }}
+          variant="outlined"
+        />
+
+        <Typography
+          fontFamily="Poppins"
+          fontSize="1.2rem"
+          sx={{ marginBottom: 1, marginTop: 4 }}
+        >
+          Not sure how to express the idea in your head? Visualize it into an
+          image.
+        </Typography>
+        <Button
+          onClick={() => {
+            alert('Not yet implemented');
+          }}
+          color="primary"
+          sx={{ height: '40px', ml: 30, mt: 2, width: '180px' }}
+          variant="contained"
+        >
+          Generate AI Image
+        </Button>
+
+        <Typography
+          fontFamily="Poppins"
+          fontSize="1.2rem"
+          sx={{ marginBottom: 3, marginTop: 6 }}
+        >
+          Enter a price range that you are comfortable with
+        </Typography>
+        <Grid alignItems="center" container spacing={10}>
+          <Grid item>
+            <Typography variant="body2">Min</Typography>
+            <TextField
+              margin="normal"
+              sx={{ width: '150px' }}
+              variant="outlined"
+            />
+          </Grid>
+          <Grid item>
+            <Typography variant="body2">Max</Typography>
+            <TextField
+              margin="normal"
+              sx={{ width: '150px' }}
+              variant="outlined"
+            />
+          </Grid>
+        </Grid>
+
+        <Typography
+          fontFamily="Poppins"
+          fontSize="1.2rem"
+          sx={{ marginBottom: 3, marginTop: 6 }}
+        >
+          Do you have any sketches that you want to share with us?
+        </Typography>
+        <Box
+          sx={{
+            border: '2px dashed #bdbdbd',
+            borderRadius: '8px',
+            cursor: 'pointer',
+            marginTop: '20px',
+            padding: '20px',
+          }}
+          onClick={handleUploadClick}
+        >
+          <Typography
+            color="textSecondary"
+            fontFamily="Poppins"
+            variant="body1"
+          >
+            Click here to upload images
+          </Typography>
+          <input
+            accept="image/*"
+            multiple
+            onChange={handleFileChange}
+            ref={fileInputRef}
+            style={{ display: 'none' }}
+            type="file"
+          />
+          <div style={{ display: 'flex', flexWrap: 'wrap', gap: '5px' }}>
+            {uploadedImages.map((image, index) => (
+              <div key={index} style={{ position: 'relative' }}>
+                <img
+                  style={{
+                    borderRadius: '8px',
+                    height: '60px',
+                    margin: '5px',
+                    objectFit: 'cover',
+                    width: '60px',
+                  }}
+                  alt={`Uploaded ${index}`}
+                  src={URL.createObjectURL(image)}
+                />
+                <div
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    handleRemoveImage(index);
+                  }}
+                  style={{
+                    alignItems: 'center',
+                    background: 'rgba(0, 0, 0, 0.5)',
+                    borderRadius: '50%',
+                    display: 'flex',
+                    height: '20px',
+                    justifyContent: 'center',
+                    padding: 0,
+                    position: 'absolute',
+                    right: 0,
+                    top: 0,
+                    width: '20px',
+                  }}
+                >
+                  <CloseIcon fontSize="small" style={{ color: 'red' }} />
+                </div>
+              </div>
+            ))}
+          </div>
+        </Box>
+
+        <Box
+          sx={{
+            border: '2px dashed #bdbdbd',
+            borderRadius: '8px',
+            cursor: 'pointer',
+            marginTop: '20px',
+            padding: '20px',
+          }}
+          onClick={handleUploadClick}
+        >
+          <Typography
+            color="textSecondary"
+            fontFamily="Poppins"
+            variant="body1"
+          >
+            Click here to upload documents
+          </Typography>
+          <input
+            multiple
+            onChange={handleFileChange}
+            ref={fileInputRef}
+            style={{ display: 'none' }}
+            type="file"
+          />
+          <div style={{ display: 'flex', flexWrap: 'wrap', gap: '5px' }}>
+            {uploadedDocuments.map((document, index) => (
+              <div key={index} style={{ position: 'relative' }}>
+                {document.type.includes('pdf') ? (
+                  <MdPictureAsPdf size={40} />
+                ) : document.type.includes('word') ||
+                  document.type.includes('document') ? (
+                  <MdDescription size={40} />
+                ) : (
+                  <MdInsertDriveFile size={40} />
+                )}
+                <div
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    handleRemoveDocument(index);
+                  }}
+                  style={{
+                    alignItems: 'center',
+                    background: 'rgba(0, 0, 0, 0.5)',
+                    borderRadius: '50%',
+                    display: 'flex',
+                    height: '20px',
+                    justifyContent: 'center',
+                    padding: 0,
+                    position: 'absolute',
+                    right: 0,
+                    top: 0,
+                    width: '20px',
+                  }}
+                >
+                  <CloseIcon fontSize="small" style={{ color: 'red' }} />
+                </div>
+              </div>
+            ))}
+          </div>
+        </Box>
+
+        <Button
+          color="primary"
+          sx={{ marginBottom: '30px', marginTop: '30px', ml: 60 }}
+          variant="contained"
+        >
+          Send Request
+        </Button>
+      </Container>
+      <Footer />
+    </>
+  );
+};
+
+export default AddCustomerRequest;
--- src/pages/customerRequests/ViewResponses.tsx
+++ src/pages/customerRequests/ViewResponses.tsx
@@ -1,56 +1,72 @@
-import React from 'react';
-import TopBar from '../components/TopAppBar';
-import Footer from '../components/Footer';
-import '../assets/font.css';
-import {
-  Typography,
-  Box,
-  Container,
-  Grid,
-  Card,
-  CardContent,
-  Button,
-} from '@mui/material';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import PhoneIcon from '@mui/icons-material/Phone';
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Container,
+  Grid,
+  Typography,
+} from '@mui/material';
+import React from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import '../../assets/font.css';
+import Footer from '../../components/Footer';
+import TopBar from '../../components/TopAppBar';
 
 const ViewResponses = () => {
+  const navigate = useNavigate();
+
+  const reqId = parseInt(useParams<{ id: string }>().id ?? '0');
+
+  const gotoResponse = (id: number) => () => {
+    navigate(`/custom-requests/req/${reqId}/responses/${id}`);
+  };
+
   const cards = [
     {
-      title: 'Response Title 1',
       amount: 'Min: LKR. 50 000 - LKR. 150 000',
+      id: 1,
       requesterName: 'UserName',
       requesterPhone: '+94 77 3829138',
+      title: 'Response Title 1',
     },
     {
+      amount: 'Min: LKR. 150 000 - LKR. 250 000',
+      id: 1,
+      requesterName: 'UserName',
+      requesterPhone: '+94 76 3829138',
       title: 'Response Title 2',
-      amount: 'Min: LKR. 150 000 - LKR. 250 000',
-      requesterName: 'UserName',
-      requesterPhone: '+94 76 3829138',
     },
     {
+      amount: 'Min: LKR. 150 000 - LKR. 250 000',
+      id: 1,
+      requesterName: 'UserName',
+      requesterPhone: '+94 76 3829138',
       title: 'Response Title 3',
-      amount: 'Min: LKR. 150 000 - LKR. 250 000',
-      requesterName: 'UserName',
-      requesterPhone: '+94 76 3829138',
     },
     {
+      amount: 'Min: LKR. 150 000 - LKR. 250 000',
+      id: 1,
+      requesterName: 'UserName',
+      requesterPhone: '+94 76 3829138',
       title: 'Response Title 4',
-      amount: 'Min: LKR. 150 000 - LKR. 250 000',
-      requesterName: 'UserName',
-      requesterPhone: '+94 76 3829138',
     },
     {
+      amount: 'Min: LKR. 150 000 - LKR. 250 000',
+      id: 1,
+      requesterName: 'UserName',
+      requesterPhone: '+94 76 3829138',
       title: 'Response Title 5',
-      amount: 'Min: LKR. 150 000 - LKR. 250 000',
-      requesterName: 'UserName',
-      requesterPhone: '+94 76 3829138',
     },
     {
-      title: 'Response Title 6',
       amount: 'Min: LKR. 150 000 - LKR. 250 000',
+      id: 1,
       requesterName: 'UserName',
       requesterPhone: '+94 76 3829138',
+      title: 'Response Title 6',
     },
   ];
   return (
@@ -58,17 +74,17 @@ const ViewResponses = () => {
       <TopBar />
       <Box style={bannerStyle}>
         <Container>
-          <Grid container justifyContent="space-between" alignItems="center">
+          <Grid alignItems="center" container justifyContent="space-between">
             <Grid item>
-              <Typography variant="h6" style={expertiseStyle}>
+              <Typography style={expertiseStyle} variant="h6">
                 Your Request,
               </Typography>
               <Typography style={needsStyle}>Their Expertise</Typography>
             </Grid>
             <Grid item>
               <img
-                src="ViewResponses.png"
                 alt="ViewResponses"
+                src="ViewResponses.png"
                 style={imageStyle}
               />
             </Grid>
@@ -78,10 +94,10 @@ const ViewResponses = () => {
       <Container>
         <Grid container spacing={10}>
           {cards.map((card, index) => (
-            <Grid item xs={12} md={6} key={index}>
+            <Grid item key={index} md={6} xs={12}>
               <Card style={cardStyle}>
                 <CardContent style={cardContentStyle}>
-                  <Typography variant="h6" style={titleStyle}>
+                  <Typography style={titleStyle} variant="h6">
                     {card.title}
                   </Typography>
                   <Typography style={amountStyle}>{card.amount}</Typography>
@@ -97,11 +113,12 @@ const ViewResponses = () => {
                   </Box>
                   <Box style={buttonContainerStyle}>
                     <Button
-                      variant="contained"
                       color="primary"
-                      style={acceptButtonStyle}
+                      onClick={gotoResponse(card.id)}
+                      style={viewButtonStyle}
+                      variant="contained"
                     >
-                      Accept response
+                      View response
                     </Button>
                   </Box>
                 </CardContent>
@@ -117,82 +134,82 @@ const ViewResponses = () => {
   );
 };
 
-const bannerStyle = {
+const bannerStyle: React.CSSProperties = {
   backgroundColor: '#F3F3F3',
   color: '#435834',
   marginBottom: '50px',
 };
 
-const expertiseStyle = {
-  fontSize: '40px',
+const expertiseStyle: React.CSSProperties = {
   fontFamily: 'Poppins',
+  fontSize: '40px',
 };
 
-const needsStyle = {
+const needsStyle: React.CSSProperties = {
+  fontFamily: 'Poppins',
   fontSize: '40px',
   marginLeft: '80px',
-  fontFamily: 'Poppins',
 };
 
-const imageStyle = {
+const imageStyle: React.CSSProperties = {
   maxWidth: '300px',
   paddingTop: '20px',
 };
 
-const cardStyle = {
+const cardStyle: React.CSSProperties = {
   border: '1px solid #ccc',
-  maxWidth: '500px',
-  maxHeight: '240px',
   margin: '0 auto',
+  maxHeight: '240px',
+  maxWidth: '500px',
 };
 
-const cardContentStyle = {
+const cardContentStyle: React.CSSProperties = {
   padding: '20px 30px',
 };
 
-const titleStyle = {
+const titleStyle: React.CSSProperties = {
+  fontFamily: 'Poppins',
   fontSize: '20px',
   fontWeight: 'bold',
-  fontFamily: 'Poppins',
+  marginBottom: '20px',
   textAlign: 'center',
-  marginBottom: '20px',
 };
 
-const amountStyle = {
-  fontSize: '18px',
+const amountStyle: React.CSSProperties = {
   fontFamily: 'Poppins',
-  textAlign: 'left',
+  fontSize: '18px',
   marginBottom: '20px',
+  textAlign: 'left',
 };
 
-const contactStyle = {
-  display: 'flex',
+const contactStyle: React.CSSProperties = {
   alignItems: 'center',
+  display: 'flex',
   marginTop: '20px',
 };
 
-const contactTextStyle = {
+const contactTextStyle: React.CSSProperties = {
   fontFamily: 'Poppins',
   fontSize: '15px',
   marginLeft: '10px',
 };
 
-const phoneIconStyle = {
+const phoneIconStyle: React.CSSProperties = {
   marginLeft: '120px',
 };
 
-const buttonContainerStyle = {
+const buttonContainerStyle: React.CSSProperties = {
+  alignItems: 'flex-start',
   display: 'flex',
   justifyContent: 'center',
-  alignItems: 'flex-start',
   marginTop: '20px',
 };
 
-const acceptButtonStyle = {
+const viewButtonStyle: React.CSSProperties = {
   marginRight: '10px',
 };
 
-const footerContainerStyle = {
+const footerContainerStyle: React.CSSProperties = {
   marginTop: '50px',
 };
 
