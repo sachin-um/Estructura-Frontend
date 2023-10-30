c754d4be6e495a969debb9ac6aa7386d70fffb48
Pamudi Guruge <92705961+Pamudi00@users.noreply.github.com>
2023-10-30 11:49:53 +0530

    Chat UI

--- src/pages/messages/Chat.tsx
+++ src/pages/messages/Chat.tsx
@@ -1,187 +1,244 @@
-import type { FormikHelpers, FormikProps } from 'formik';
-
-import { Button, TextField } from '@mui/material';
-import { Form, Formik } from 'formik';
-import { useCallback, useEffect, useRef, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import * as Yup from 'yup';
-
-import TimeAgo from '../../components/TimeAgo';
-import TopAppBar from '../../components/TopAppBar';
-import useMessages from '../../hooks/messages/useMessages';
-import useCurrentUser from '../../hooks/users/useCurrentUser';
-import useFetchUser from '../../hooks/users/useFetchUser';
-import GetFormikProps from '../../utils/GetFormikProps';
+import {
+  Avatar,
+  Box,
+  Button,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Paper,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { AttachFile, InsertDriveFile } from '@mui/icons-material';
+import { useState, useEffect } from 'react';
 
 const Chat = () => {
-  const [sendCheck, setSend] = useState(1);
-  const recipientId = parseInt(useParams<{ id: string }>().id ?? '0');
-  const { fetchMessages, messageList, seen, sendMessage } = useMessages();
-  const currentUser = useCurrentUser();
-  const { fetchUserById, user } = useFetchUser();
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+  const [files, setFiles] = useState([]);
+  const [fileInputKey, setFileInputKey] = useState(0);
 
-  useEffect(() => {
-    fetchUserById(recipientId);
-  }, [fetchUserById, recipientId]);
+  const formatDate = (date) => {
+    const today = new Date();
+    const messageDate = new Date(date);
 
-  const fetcher = useCallback(() => {
-    fetchMessages(recipientId);
-  }, [fetchMessages, recipientId]);
+    if (
+      today.getFullYear() === messageDate.getFullYear() &&
+      today.getMonth() === messageDate.getMonth() &&
+      today.getDate() === messageDate.getDate()
+    ) {
+      return 'Today';
+    }
 
-  useEffect(() => {
-    fetcher();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [sendCheck]);
+    today.setDate(today.getDate() - 1);
+    if (
+      today.getFullYear() === messageDate.getFullYear() &&
+      today.getMonth() === messageDate.getMonth() &&
+      today.getDate() === messageDate.getDate()
+    ) {
+      return 'Yesterday';
+    }
 
-  useEffect(() => {
-    setTimeout(fetcher, 5000);
-  });
+    return messageDate.toDateString();
+  };
 
-  useEffect(() => {
-    setTimeout(() => {
-      if (document.hasFocus()) {
-        messageList?.forEach((message) => {
-          if (!message.seen && message.recipientId === currentUser?.id) {
-            seen(message.id);
-            setSend(sendCheck - 1);
-          }
-        });
-      }
-    }, 3000);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [messageList]);
+  const isImage = (file) => {
+    return file.type.startsWith('image/');
+  };
 
-  const FormRef = useRef<FormikProps<SendMessageRequest>>(null);
-  const fileRef = useRef<HTMLInputElement>(null);
+  const handleSendMessage = () => {
+    if (message.trim() === '' && files.length === 0) return;
 
-  const HandleSubmit = (
-    values: SendMessageRequest,
-    formikHelpers: FormikHelpers<SendMessageRequest>,
-  ) => {
-    if (FormRef.current !== null) {
-      const { setSubmitting } = FormRef.current;
-      setSubmitting(true);
-      sendMessage(recipientId, values).then((added) => {
-        console.log('added', added.data);
-        FormRef.current?.setValues(initialValues);
-        formikHelpers.resetForm();
-        setSend(sendCheck + 1);
-      });
-      setSubmitting(false);
+    // Create separate messages for each selected file
+    files.forEach((file) => {
+      const fileMessage = {
+        file: file,
+        sender: 'You',
+        timestamp: new Date().toISOString(),
+      };
+      setMessages((prevMessages) => [...prevMessages, fileMessage]);
+    });
+
+    if (message.trim() !== '') {
+      const textMessage = {
+        content: message,
+        sender: 'You',
+        timestamp: new Date().toISOString(),
+      };
+      setMessages([...messages, textMessage]);
+    }
+
+    setMessage('');
+    setFiles([]);
+    setFileInputKey(fileInputKey + 1);
+  };
+
+  const handleFileChange = (e) => {
+    const selectedFiles = Array.from(e.target.files);
+    setFiles([...files, ...selectedFiles]);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleSendMessage();
     }
   };
 
-  const validationSchema = Yup.object().shape({
-    content: Yup.string(),
-    files: Yup.mixed().test(
-      'fileSize',
-      'Each File size should be less than 5MB',
-      (value) => {
-        const fileArr = value as FileList;
-        let totalSize = 0;
-        for (let index = 0; index < fileArr.length; index++) {
-          const element = fileArr[index];
-          if (element.size > 5000000) {
-            console.log('too large');
-            return false;
-          }
-          totalSize += element.size;
-        }
-        if (totalSize > 20000000) {
-          console.log('too large');
-          return false;
-        }
-        return true;
-      },
-    ),
-  });
+  useEffect(() => {
+    document.addEventListener('keydown', (e) => {
+      if (e.key === 'Enter') {
+        handleSendMessage();
+      }
+    });
 
-  const initialValues: SendMessageRequest = {
-    content: '',
-    files: new DataTransfer().files,
-  };
+    return () => {
+      document.removeEventListener('keydown', handleSendMessage);
+    };
+  }, []);
 
   return (
-    <>
-      <TopAppBar />
-      <div>
-        <h1>
-          Chat with {user?.firstName + ' ' + user?.lastName} [{user?.role}]
-        </h1>
-        {messageList?.map((m) => (
-          <div
-            style={{
-              backgroundColor:
-                m.senderId === currentUser?.id ? 'palegreen' : 'paleturquoise',
-            }}
-            key={m.createdAt.toString()}
-          >
-            from : {m.senderId} <br />
-            to : {m.recipientId} <br />
-            content : {m.content} <br />
-            files :
-            {m.files.map((f, i) => (
-              <a
-                href={`http://localhost:8080/files/message-files/${m.senderId}/${m.recipientId}/${f}`}
-                key={f}
-              >
-                {m.fileOriginalNames[i]}
-              </a>
-            ))}
-            <br />
-            created: {TimeAgo({ timestamp: m.createdAt })}
-            {m.seen && 'seen'}
-          </div>
-        ))}
-      </div>
-      <Formik
-        enableReinitialize
-        initialValues={initialValues}
-        innerRef={FormRef}
-        onSubmit={HandleSubmit}
-        validationSchema={validationSchema}
+    <div
+      style={{
+        backgroundColor: '#F3F3F3',
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100vh',
+      }}
+    >
+      <Box sx={{ flex: 1, overflowY: 'scroll' }}>
+        <List>
+          {messages.map((msg, index) => (
+            <div key={index}>
+              {index === 0 ||
+              formatDate(msg.timestamp) !==
+                formatDate(messages[index - 1].timestamp) ? (
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                  <Typography
+                    style={{
+                      backgroundColor: 'lightgray',
+                      borderRadius: '4px',
+                      fontSize: '16px',
+                      padding: '4px',
+                    }}
+                    align="center"
+                    variant="caption"
+                  >
+                    {formatDate(msg.timestamp)}
+                  </Typography>
+                </div>
+              ) : null}
+              <ListItem alignItems="flex-start">
+                {msg.sender !== 'You' && (
+                  <ListItemAvatar>
+                    <Avatar sx={{ height: 40, width: 40 }}>
+                      {msg.sender[0]}
+                    </Avatar>
+                  </ListItemAvatar>
+                )}
+                <Paper
+                  style={{
+                    backgroundColor:
+                      msg.sender === 'You' ? '#E7C4A0' : '#9D6432',
+                    borderRadius: '10px',
+                    marginLeft: msg.sender === 'You' ? 'auto' : 'inherit',
+                    marginRight: msg.sender !== 'You' ? 'auto' : 'inherit',
+                    maxWidth: '70%',
+                    padding: '10px',
+                    position: 'relative',
+                  }}
+                  elevation={3}
+                >
+                  <div>
+                    <ListItemText
+                      primary={msg.sender}
+                      secondary={msg.content}
+                    />
+                    {msg.file ? (
+                      <div>
+                        {isImage(msg.file) ? (
+                          <div style={{ width: '150px' }}>
+                            <img
+                              src={URL.createObjectURL(msg.file)}
+                              alt="Sent Image"
+                              style={{ width: '100%', height: 'auto', borderRadius: '10px' }}
+                            />
+                          </div>
+                        ) : (
+                          <a
+                            href={URL.createObjectURL(msg.file)}
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            <Typography variant="body2" style={{ display: 'flex', alignItems: 'center' }}>
+                              <InsertDriveFile style={{ marginRight: '4px', color: 'darkgreen' }} />
+                              {msg.file.name}
+                            </Typography>
+                          </a>
+                        )}
+                      </div>
+                    ) : null}
+                    <Typography
+                      style={{
+                        color: 'gray',
+                        fontSize: '12px',
+                        textAlign: 'right',
+                      }}
+                      variant="body2"
+                    >
+                      {new Date(msg.timestamp).toLocaleTimeString([], {
+                        hour: '2-digit',
+                        minute: '2-digit',
+                      })}
+                    </Typography>
+                  </div>
+                </Paper>
+              </ListItem>
+            </div>
+          ))}
+        </List>
+      </Box>
+      <Box
+        alignItems="center"
+        borderTop="1px solid lightgray"
+        display="flex"
+        p={1}
       >
-        {(FormikProps: FormikProps<SendMessageRequest>) => {
-          const spread = GetFormikProps(FormikProps);
-          return (
-            <Form onSubmit={FormikProps.handleSubmit}>
-              <TextField type="text" {...spread('content')} />
-              {FormikProps.errors.files?.toString()}
-              <input
-                onChange={(event) => {
-                  if (event.target.files !== null) {
-                    FormikProps.setFieldValue(
-                      'files',
-                      event.target.files,
-                      true,
-                    );
-                  } else {
-                    FormikProps.setFieldValue(
-                      'files',
-                      new DataTransfer().files,
-                      true,
-                    );
-                  }
-                }}
-                multiple
-                name="files"
-                ref={fileRef}
-                type="file"
-              ></input>
-              <Button
-                color="primary"
-                fullWidth
-                sx={{ borderRadius: 2, width: 1 / 3 }}
-                type="submit"
-                variant="contained"
-              >
-                Send
-              </Button>
-            </Form>
-          );
-        }}
-      </Formik>
-    </>
+        <TextField
+          fullWidth
+          label="Type a message"
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyPress={handleKeyPress}
+          value={message}
+          variant="outlined"
+        />
+        <input
+          type='file'
+          accept='image/*, .pdf, .doc, .docx'
+          style={{ display: 'none' }}
+          onChange={handleFileChange}
+          id='fileInput'
+          key={fileInputKey}
+          multiple
+        />
+        <label htmlFor='fileInput' style={{ cursor: 'pointer' }}>
+          <AttachFile fontSize='large' />
+        </label>
+        {files.length > 0 && (
+          <Typography variant="body2">
+            Selected Files: {files.map(file => file.name).join(', ')}
+          </Typography>
+        )}
+        <Button
+          color="primary"
+          onClick={handleSendMessage}
+          style={{ marginLeft: '10px' }}
+          variant="contained"
+        >
+          Send
+        </Button>
+      </Box>
+    </div>
   );
 };
 
